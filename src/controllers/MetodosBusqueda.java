aacc56e1be3198e4656a47ec97864071968c4338
Darlex372 <darlex2704@gmail.com>
2025-04-16 16:15:44 -0500

    hola

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -10,42 +10,61 @@ public class MetodosBusqueda
 
     public MetodosBusqueda(Persona[] persons){
         showConsole = new ShowConsole();
-        this.people=persons;
-        showPerson();
+        this.people = persons;
+        showMenu();
     }
-    public int busquedaLineal(int[] arreglo, int valor) 
-    {
-        for (int i = 0; i < arreglo.length; i++) 
-        {
-            if (arreglo[i] == valor) 
-            {
-                return i;
-            }
+
+    public void showMenu() {
+        int option = showConsole.inputSearchOption();
+        if (option == 1) {
+            showPersonByCode();
+        } else if (option == 2) {
+            showPersonByName();
+        } else {
+            showConsole.showMessage("Opción inválida");
         }
-        return -1;
     }
 
     public int findPersonByCode(int code){
         for (int i = 0; i < people.length; i++) {
-            if (people[i].getCode() == code) 
-            {
+            if (people[i].getCode() == code) {
                 return i;
             }
-            
         }
         return -1;
     }
 
-    public void showPerson(){
-        int codeTofind = showConsole.inputCode();
-        int indexPerson = findPersonByCode(codeTofind);
+    public int findPersonByName(String name){
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].getName().equalsIgnoreCase(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void showPersonByCode(){
+        int codeToFind = showConsole.inputCode();
+        int indexPerson = findPersonByCode(codeToFind);
         
         if (indexPerson >= 0){
-            showConsole.showMessage("Persona con codigo " + codeTofind + " encontada");
+            showConsole.showMessage("Persona con código " + codeToFind + " encontrada");
             showConsole.showMessage(people[indexPerson].toString());
-        }else{
+        } else {
             showConsole.showMessage("Persona no encontrada");
         }
+    }
 
+    public void showPersonByName(){
+        String nameToFind = showConsole.inputName();
+        int indexPerson = findPersonByName(nameToFind);
+        
+        if (indexPerson >= 0){
+            showConsole.showMessage("Persona con nombre '" + nameToFind + "' encontrada");
+            showConsole.showMessage(people[indexPerson].toString());
+        } else {
+            showConsole.showMessage("Persona no encontrada");
+        }
     }
 }
+
--- src/views/ShowConsole.java
+++ src/views/ShowConsole.java
@@ -26,5 +26,18 @@ public class ShowConsole
         System.out.println(message);
     }
     
+    public int inputSearchOption(){
+        System.out.println("Seleccione el tipo de búsqueda:");
+        System.out.println("1. Buscar por código");
+        System.out.println("2. Buscar por nombre");
+        return scanner.nextInt();
+    }
+    
+    public String inputName(){
+        System.out.println("Ingrese un nombre");
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+    
 
 }
