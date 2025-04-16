0c59a3227a7cc4d3cbecbd1b8fe0c2592403548e
Darlex372 <darlex2704@gmail.com>
2025-04-16 18:00:58 -0500

    hola

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import controllers.MetodosBusqueda;
+import controllers.MetodosBusquedaBinaria;
 import models.Persona;
 
 public class App 
@@ -6,15 +6,15 @@ public class App
     public static void main(String[] args)
     {
         Persona[] personas = new Persona[7];
-        personas[0] = new Persona(1, "dennis");
-        personas[1] = new Persona(2, "flopi");
-        personas[2] = new Persona(3, "mauro");
-        personas[3] = new Persona(4, "carmen");
-        personas[4] = new Persona(5, "wilson");
-        personas[5] = new Persona(6, "castro");
-        personas[6] = new Persona(7, "devora");
-
-        MetodosBusqueda mB = new MetodosBusqueda(personas);
-        
+        personas[0] = new Persona(101, "dennis");
+        personas[1] = new Persona(102, "flopi");
+        personas[2] = new Persona(103, "mauro");
+        personas[3] = new Persona(104, "carmen");
+        personas[4] = new Persona(105, "wilson");
+        personas[5] = new Persona(106, "castro");
+        personas[6] = new Persona(107, "devora");
+        //MetodosBusqueda mB = new MetodosBusqueda(personas);
+        MetodosBusquedaBinaria mBB = new MetodosBusquedaBinaria(personas);
+        mBB.showPerson();
     }
 }
